716be7c54916d79f7d6a41da492e46d94e819eb1
SaptarshiMazumder <48704857+SaptarshiMazumder@users.noreply.github.com>
2025-03-17 23:02:23 +0900

    made a basic test case for api view

--- users/test_api.py
+++ users/test_api.py
@@ -1,50 +1,84 @@
 
-# from rest_framework.test import APITestCase
-# from django.urls import reverse
-# from .models import User, Pref
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .models import Pref
 
-# class UserRegistrationAPITest(APITestCase):
-#     def setUp(self):
-#         self.pref = Pref.objects.create(name="Kyoto")
-#         self.url = reverse('register')
+User = get_user_model()
 
-#     def test_registration_success(self):
-#         data = {
-#             'username': 'abc',
-#             'email': 'abc@gmail.com',
-#             'password': 'Japan123',
-#             'password_confirm': 'Japan123',
-#             'tel': '1234567899',
-#             'pref': self.pref.id,
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, 201)
-#         self.assertTrue(User.objects.filter(email='apiuser@example.com').exists())
-    
-#     def test_registration_invalid_email(self):
-#         data = {
-#             'username': 'xyz',
-#             'email': 'xyz@gmail',
-#             'password': 'Japan123',
-#             'password_confirm': 'Japan123',
-#             'tel': '5551234567',
-#             'pref': self.pref.id,
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         print(response.data)
+class PrefAPIViewTest(APITestCase):
 
-#         self.assertEqual(response.status_code, 400)
-#         self.assertIn('email', response.data)
-    
-#     def test_password_mismatch(self):
-#         data = {
-#             'username': 'pqr',
-#             'email': 'pqr@gmail.com',
-#             'password': 'Japan123',
-#             'password_confirm': 'Japan',
-#             'tel': '5551234567',
-#             'pref': self.pref.id,
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, 400)
-#         self.assertIn('password_confirm', response.data)
+    def setUp(self):
+        self.pref1 = Pref.objects.create(name="Tokyo")
+        self.pref2 = Pref.objects.create(name="Osaka")
+
+    def test_get_prefectures(self):
+        url = reverse('get_prefectures')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        json_data = response.json()
+
+        self.assertIn('prefectures', json_data)
+        self.assertEqual(len(json_data['prefectures']), 2)
+
+
+        pref_names = {pref['name'] for pref in json_data['prefectures']}
+        self.assertSetEqual(pref_names, {"Tokyo", "Osaka"})
+
+class UserRegistrationAPIViewTest(APITestCase):
+    def setUp(self):
+        self.pref = Pref.objects.create(name="Tokyo")
+        self.api_register_url = reverse('api_register')
+
+    def test_successful_registration(self):
+        data = {
+
+            'username': 'naruto_uzumaki',
+            'email': 'hokage.naruto@gmail.com',
+            'password': 'Rasengan99',
+            'password_confirm': 'Rasengan99',
+            'tel': '1234567890',
+            'pref': self.pref.id,
+        }
+
+        response = self.client.post(self.api_register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], data['username'])
+        self.assertEqual(response.data['email'], data['email'])
+        self.assertTrue(User.objects.filter(email=data['email']).exists())
+
+    def test_password_mismatch(self):
+        data = {
+
+            'username': 'ichigo_k',
+            'email': 'ichigo.soulreaper@yahoo.com',
+            'password': 'Zangetsu22',
+            'password_confirm': 'Bankai22',
+            'tel': '1234567890',
+            'pref': self.pref.id,
+        }
+        response = self.client.post(self.api_register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertIn('password_confirm', response.data)
+
+    def test_duplicate_email(self):
+        User.objects.create_user(
+            username='lelouch_v2',
+            email='zero@geass.com',
+            password='CodeGeass01'
+        )
+
+        data = {
+            'username': 'lelouch_v2',
+            'email': 'zero@geass.com',
+            'password': 'CodeGeass01',
+            'password_confirm': 'CodeGeass01',
+            'tel': '1234567890',
+            'pref': self.pref.id,
+        }
+        response = self.client.post(self.api_register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('email', response.data)
